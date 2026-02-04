089289f4f63deac18f7a25af57ea458e487fb649
hee128 <eum.jin.hee8865@gmail.com>
2026-02-04 14:15:07 +0900

    Update index.js

--- index.js
+++ index.js
@@ -6,23 +6,21 @@ new fullpage('#fullpage', {
     scrollingSpeed: 1000
 });
 
-// 반응형 모드
-        const toggleBtn1 = document.querySelector(".toggleBtn1");
-        const pic1 = document.querySelector(".pic1");
-        const pic2 = document.querySelector(".pic2");
 
-        toggleBtn1.addEventListener("click", ()=>{
-            pic1.classList.toggle("active"); /*active가 없으면 추가해주고*/
-            pic2.classList.toggle("active"); /*active가 있다면 빼준다*/
-        })
+const aboutBtn = document.querySelector('.about_btn');
+const about2 = document.querySelector('#about');
+aboutBtn.addEventListener('click',() => {
+   about2.classList.add('open')
+})
 
-        const toggleBtn2 = document.querySelector(".toggleBtn2");
-        const pic3 = document.querySelector(".pic3");
-        const pic4 = document.querySelector(".pic4");
 
-        toggleBtn2.addEventListener("click", ()=>{
-            pic3.classList.toggle("active"); /*active가 없으면 추가해주고*/
-            pic4.classList.toggle("active"); /*active가 있다면 빼준다*/
-        })
-        
+const xBtn = document.querySelector('.x_btn');
+const about = document.querySelector('.about');
+
+xBtn.addEventListener('click',() => {
+   about.classList.remove('open')
+})
+
+
+
 
